7538a6b53a8cafd943224ddbebce8e70fccf07a3
JinseongHwang <eddy5360@naver.com>
2022-05-03 01:03:26 +0900

    [실버 5] 수 정렬하기 2 https://www.acmicpc.net/problem/2751

---
+++ 황진성/백준/2751.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(int(input()))
+
+data.sort()
+
+for d in data:
+    print(d)
