443ce24c76c04cf8ec1fb6a17fc2a79fde879ca4
Eunbyul <89649741+EunByu1@users.noreply.github.com>
2022-05-17 17:32:30 +0900

    [브론즈 1] 팰린드롬인지 확인하기
    https://www.acmicpc.net/problem/10988

--- 하은지/백준/01. 언어 학습을 위한 기초문제/10998.py
+++ 하은지/백준/01. 언어 학습을 위한 기초문제/10998.py
@@ -1,5 +1,22 @@
 # [0] 변수 선언부 
-A, B = map(int, input().split())
+word = input()
+  # 정방향으로 읽은 단어를 저장하기 위한 리스트 
+Forward_word   = []
+  # 역방향으로 읽은 단어를 저장하기 위한 리스트 
+Backward_word  = []
 
-# [1] 연산 결과 출력
-print(A*B)
+
+# [1] 리스트 & 값 바인딩  
+for i in range(len(word)):
+    Forward_word.append(word[i])
+
+for i in range(len(word)-1, -1, -1):
+    Backward_word.append(word[i])
+
+ 
+# [2] 정방향으로 읽은 단어와 역방향으로 읽은 단어 비교 후 출력
+if (Forward_word == Backward_word):
+    print(1)    # 팰린드롬
+    
+else:
+    print(0)    # 팰린드롬 X
