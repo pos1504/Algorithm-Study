b865d56429521e864445d615855205af9efd12f2
Yuhee Kim <dbgml4509@naver.com>
2022-05-21 22:55:36 +0900

    [실버 4] 카드
    https://www.acmicpc.net/problem/11652

---
+++ 김유희/백준/11652.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cards = {}
+
+for i in range(N):
+    num = int(input())
+    if num in cards :
+        cards[num] += 1
+    else :
+        cards[num] = 1
+
+cards = sorted(cards.items(),key = lambda x : (-x[1], x[0]))
+print(cards[0][0])
